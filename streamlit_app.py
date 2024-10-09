f6229485da765b8d6596d081b3853cef363a78a6
trollingmonk <rahulpawar890@gmail.com>
2024-10-10 00:33:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if file_image:
     with two:
         st.write("**Output Pencil Sketch**")
         st.image(final_sketch, use_column_width=True)
-    if st.button("I would like to Download Sketch Images"):
+    #if st.button("I would like to Download Sketch Images"):
         im_pil = Image.fromarray(final_sketch)
         im_pil.save('final_image.jpeg')
     with open('final_image.jpeg', "rb") as file:
