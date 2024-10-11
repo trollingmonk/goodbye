141c5d1088575cec902f4a388c66a23e545a384b
trollingmonk <rahulpawar890@gmail.com>
2024-10-11 22:41:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,51 +4,6 @@ from PIL import Image
 import cv2
 from pymongo import MongoClient
 
-
-"""
-def dodgeV2(x, y):
-    return cv2.divide(x, 255 - y, scale=256)
-
-def pencilsketch(inp_img):
-    img_gray = cv2.cvtColor(inp_img, cv2.COLOR_BGR2GRAY)
-    img_invert = cv2.bitwise_not(img_gray)
-    img_smoothing = cv2.GaussianBlur(img_invert, (21, 21),sigmaX=0, sigmaY=0)
-    final_img = dodgeV2(img_gray, img_smoothing)
-    return(final_img)
-
-
-st.title("Smile at camera 📷")
-st.write("This is your chance to say Goodbye to Rahul Pawar and Wish him Best of Luck")
-
-# collecting the input image from user camera 
-
-file_image = st.camera_input(label = "Take a pic of you to be sketched out",label_visibility="hidden")
-
-if file_image:
-    input_img = Image.open(file_image)
-    final_sketch = pencilsketch(np.array(input_img))
-    one, two = st.columns(2)
-    with one:
-        st.write("**Input Photo**")
-        st.image(input_img, use_column_width=True)
-    with two:
-        st.write("**Output Pencil Sketch**")
-        st.image(final_sketch, use_column_width=True)
-    im_pil = Image.fromarray(final_sketch)
-    im_pil.save("final_image.jpeg")
-    with open("final_image.jpeg", "rb") as file:
-        st.download_button(
-            label="Download",
-            data=file,
-            file_name="final_image.jpeg",
-            mime="image/png")
-    
-
-  
-#else:
-#     st.write("Oooh something wrong with your Camera")
-"""
-
 ATLAS_URI = "mongodb+srv://sicaga9567:Pohapoha123@cluster0.nb0qv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 class AtlasClient ():
@@ -71,3 +26,5 @@ COLLECTION_NAME = 'embedded_movies'
 atlas_client = AtlasClient (ATLAS_URI, DB_NAME)
 atlas_client.ping()
 print ('Connected to Atlas instance! We are good to go!')
+movies = atlas_client.find (collection_name=COLLECTION_NAME, limit=5)
+print (f"Found {len (movies)} movies")
