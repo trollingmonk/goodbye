d5c397ae1be9c3699b9900628d300f6d235d6a49
trollingmonk <rahulpawar890@gmail.com>
2024-10-12 17:37:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,23 +28,16 @@ st.write("This is your chance to say Goodbye to Rahul Pawar and Wish him Best of
 
 file_image = st.camera_input(label = "Take a pic of you to be sketched out",label_visibility="hidden")
 
-if file_image:
-    im = Image.open(file_image)
-    image_bytes = io.BytesIO()
-    im.save(image_bytes, format='JPEG')
-    image = {'data': image_bytes.getvalue()}
-    image_id = images.insert_one(image).inserted_id
-    
+if file_image:  
     atlas_uri="mongodb+srv://sicaga9567:pohapoha123@cluster0.nb0qv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     db_name='imagedb'
     COLLECTION_NAME='images'
     atlas_client=AtlasClient(atlas_uri,db_name)
     images=atlas_client.get_collection(collection_name=COLLECTION_NAME)
     
-    im=Image.open("input_img.jpeg")
+    im=Image.open(file_image)
     image_bytes=io.BytesIO()
     im.save(image_bytes, format='JPEG')
-    
     image={'data': image_bytes.getvalue()}
     image_id=images.insert_one(image).inserted_id
     
