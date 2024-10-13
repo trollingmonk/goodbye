5142a13e8278f43b105742f21974b880636e4fae
trollingmonk <rahulpawar890@gmail.com>
2024-10-14 00:42:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,6 @@ class AtlasClient():
        items = collection.find(filter=filter, limit=limit)
        return items
 
-atlas_uri="mongodb+srv://sicaga9567:pohapoha123@cluster0.nb0qv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-db_name='imagedb'
-COLLECTION_NAME='images'
-atlas_client=AtlasClient(atlas_uri,db_name)
-images=atlas_client.get_collection(collection_name=COLLECTION_NAME)
-result = list(images.find())
-
 st.title("Smile at camera 📷")
 st.write("This is your chance to say Goodbye to Rahul Pawar and Wish him Best of Luck")
 
@@ -42,38 +35,15 @@ with st.container():
         image={'data': image_bytes.getvalue()}
         image_id=images.insert_one(image).inserted_id
 
-if 'count' not in st.session_state:
-    st.session_state.count = 0
-
-if 'quotes' not in st.session_state:
-    st.session_state.quotes = result
-
-def display_quote():
-    quote = st.session_state.quotes[st.session_state.count]
-    #st.write(quote)
-    pil_img=Image.open(io.BytesIO(quote['data']))
-    st.image(pil_img)
-
-def next_quote():
-    if st.session_state.count + 1 >= len(st.session_state.quotes):
-        st.session_state.count = 0
-    else:
-        st.session_state.count += 1
-
-def previous_quote():
-    if st.session_state.count > 0:
-        st.session_state.count -= 1
-
 with st.container():
-    st.title("Imgaes")
-    display_quote()
+    atlas_uri="mongodb+srv://sicaga9567:pohapoha123@cluster0.nb0qv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    db_name='imagedb'
+    COLLECTION_NAME='images'
+    atlas_client=AtlasClient(atlas_uri,db_name)
+    images=atlas_client.get_collection(collection_name=COLLECTION_NAME)
+    result = list(images.find())
+    for i in range(len(result)):
+        pil_img=Image.open(io.BytesIO(i['data']))
+        st.image(pil_img)
+        
 
-    col1, col2 = st.columns(2)
-
-    with col1:
-        if st.button("⏮️ Previous", on_click=previous_quote):
-            pass
-
-    with col2:
-        if st.button("Next ⏭️", on_click=next_quote):
-           pass
