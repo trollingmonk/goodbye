d55ba4b3f5c67afb8aef3c9784087edbbaa40c93
trollingmonk <rahulpawar890@gmail.com>
2024-10-17 23:34:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,19 +21,18 @@ class AtlasClient():
 st.title("Smile at camera 📷")
 st.write("This is your chance to say Goodbye to Rahul Pawar and Wish him Best of Luck")
 
-with st.container():
-    file_image = st.camera_input(label = "Take a pic of you to be sketched out",label_visibility="hidden")
-    if file_image:
-        atlas_uri="mongodb+srv://sicaga9567:pohapoha123@cluster0.nb0qv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-        db_name='imagedb'
-        COLLECTION_NAME='images'
-        atlas_client=AtlasClient(atlas_uri,db_name)
-        images=atlas_client.get_collection(collection_name=COLLECTION_NAME)
-        im=Image.open(file_image)
-        image_bytes=io.BytesIO()
-        im.save(image_bytes, format='JPEG')
-        image={'data': image_bytes.getvalue()}
-        image_id=images.insert_one(image).inserted_id
+file_image = st.camera_input(label = "Take a pic of you to be sketched out",label_visibility="hidden")
+if file_image:
+    atlas_uri="mongodb+srv://sicaga9567:pohapoha123@cluster0.nb0qv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    db_name='imagedb'
+    COLLECTION_NAME='images'
+    atlas_client=AtlasClient(atlas_uri,db_name)
+    images=atlas_client.get_collection(collection_name=COLLECTION_NAME)
+    im=Image.open(file_image)
+    image_bytes=io.BytesIO()
+    im.save(image_bytes, format='JPEG')
+    image={'data': image_bytes.getvalue()}
+    image_id=images.insert_one(image).inserted_id
 		
 # Function to retrieve images
 def get_images():
